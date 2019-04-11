94f0910b37dd697ccd96fc50d7b72dbcdd44e41d
Aman Bhargava <amanb2000@gmail.com>
2019-04-10 21:13:57 -0400

    Submitted this version

--- cfd_burger_anim50.py
+++ cfd_burger_anim50.py
@@ -1,4 +1,5 @@
 import numpy as numpy
+import matplotlib
 from matplotlib import pyplot, cm
 from matplotlib.colors import Normalize
 
@@ -81,15 +82,15 @@ def evolve(u, v, steps):
 		(u, v) = equation_of_motion(u, v)
 		(u, v) = boundary(u, v, nozzle_u, nozzle_v, nx, ny, i)
 
+		if(i%50 == 0):
+			magnitude = numpy.sqrt(u[::2]**2 + v[::2]**2)
+			pyplot.quiver(u[::2], v[::2], norm(magnitude), scale = 60,
+			cmap=pyplot.cm.jet)
+			ax.savefig('frame'+str(i).zfill(5)+'.png',dpi=300)
+			ax.clear()
 	return(u, v)
 
 (final_u, final_v) = evolve(initial_u, initial_v, nt)
-
-magnitude = numpy.sqrt(final_u[::2]**2 + final_v[::2]**2)
-pyplot.quiver(final_u[::2], final_v[::2], norm(magnitude), scale = 60,
-cmap=pyplot.cm.jet)
-ax.savefig('frame'+str(nt).zfill(5)+'.png',dpi=300)
-ax.clear()
 
 print(final_u)
 print(final_v)
